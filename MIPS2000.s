d2861452bb74c941f5945d15756ab3048a413ded
Thomas paganelli <thomas.paganelli03@gmail.com>
2023-05-14 15:16:47 +0200

    aggiunti cicli di blink del led

--- MIPS2000.s
+++ MIPS2000.s
@@ -94,33 +94,50 @@ routine15_adress:
 #Effettuo il complemento bit a bit dei 4 bit meno significativi
 
 main:
- li $t1, START
- li $t2, COMMAND
- li $t3, LED
+ la $t1, START
+ la $t2, COMMAND
+ la $t3, LED
 
-#Estrazione del comando COMMAND nei 2 nybble rispettivamente più significativi e meno significativi.
-#Nel nybble meno significativi, effettuo il complemento bit a bit
+# Estrazione del comando COMMAND nei 2 nybble rispettivamente più significativi e meno significativi.
+# Nel nybble meno significativi, effettuo il complemento bit a bit
 
 lb $t4, 0($t2)          # esempio:                                1110 0001  
-andi $t4, $t2, 0x0F     #Estraggo il nybble piu significativo --> 0000 0001
-andi $t5, $t2, 0xF0     #Estraggo il nybble meno significativo --> 1110 0000
+andi $t4, $t2, 0x0F     # Estraggo il nybble piu significativo --> 0000 0001
+andi $t5, $t2, 0xF0     # Estraggo il nybble meno significativo --> 1110 0000
 
-xor	$t5, $t5, $t4   # --> 1110 0001 , mi permette di fare il complemento bit a bit.
+xor	$t5, $t5, $t4       # --> 1110 0001 , mi permette di fare il complemento bit a bit.
 
-bne $t4, $t5, errore    #bisogna gestire l'errore
+bne $t4, $t5, errore    # Bisogna gestire l'errore
 
 
 sll $t4, $t4, 2
 
 
-
-# Il comando non è corretto, inibisce l'accettazione di dati per 60 secondi
 errore:
-li $t6, 60    # Carica il conteggio di 60 secondi in $t6
+    li $t1, 60          # Imposta il contatore a 60 (numero di secondi)
+    li $t4, 2           # Imposta il secondo contatore a 2 (numero di secondi)
+
+led_loop:
+    sw $zero, 0($t0)    # Scrive il valore 0 nella cella LED (led spento)
+    move $t3, $zero     # Imposta il registro $t3 a 0
+    sw $ra, 4($sp)
+    jal delay           # Chiamata alla funzione delay per un ritardo di 2 secondi
+
+    li $t3, 0x8000      # Imposta il valore 0x8000 nel registro $t3 (led acceso)
+    sw $t3, 0($t0)      # Scrive il valore 0x8000 nella cella LED (led acceso)
+    jal delay           # Chiamata alla funzione delay per un ritardo di 2 secondi
+
+    addi $t1, $t1, -2   # Sottrae 2 dal contatore dei secondi rimanenti
+    beq $t1, $zero, end_loop # Salta al ciclo del led se il contatore non è zero
+    jal delay_loop
 
 
-# Effettua il lampeggio
-lampeggio:
-li $t0, 15000000000
-loop: addi $t0, $t0, -1
-bne $t0, $zero, loop
+delay_loop:
+    addi $t4, $t4, -400  # Decrementa il contatore del ritardo di (1/500000000)*2 secondi, espresso in microsecondi
+    bnez $t4, delay_loop # Ripeti il ciclo finché il contatore non è zero
+
+
+end_loop:
+    lw $ra, 4($sp)
+    jr $ra               # Ritorna alla chiamata della funzione
+
