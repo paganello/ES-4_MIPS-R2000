ee789e7dbf6cc2568e394d0a147dd2ff75e23698
Thomas paganelli <thomas.paganelli03@gmail.com>
2023-05-14 12:06:28 +0200

    first commit

---
+++ MIPS2000.s
@@ -0,0 +1,58 @@
+.data
+COMMAND_ADDR:   .byte   0
+START_ADDR:     .word   0
+LED_ADDR:       .half   0
+
+COMMAND_TABLE:
+    .word   routine0_adress 
+    .word   routine1_adress
+    .word   routine2_adress
+    .word   routine3_adress
+    .word   routine4_adress
+    .word   routine5_adress
+    .word   routine6_adress
+    .word   routine7_adress
+    .word   routine8_adress
+    .word   routine9_adress
+    .word   routine10_adress
+    .word   routine11_adress
+    .word   routine12_adress
+    .word   routine13_adress
+    .word   routine14_adress
+    .word   routine15_adress    
+
+.text
+routine0_adress:
+    #codice routine 0
+routine1_adress:
+    #cordice routine 2
+routine2_adress:
+    #cordice routine 2
+routine3_adress:
+    #cordice routine 2
+routine4_adress:
+    #cordice routine 2
+routine5_adress:
+    #cordice routine 
+routine6_adress:
+    #cordice routine 
+routine7_adress:
+    #cordice routine 
+routine8_adress:
+    #cordice routine 
+routine9_adress:
+    #cordice routine 
+routine10_adress:
+    #cordice routine 
+routine11_adress:
+    #cordice routine 
+routine12_adress:
+    #cordice routine 
+routine13_adress:
+    #cordice routine 
+routine14_adress:
+    #cordice routine 
+routine15_adress:
+    #cordice routine 
+
+main:
