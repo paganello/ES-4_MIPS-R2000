50c70380cdbeef9f6e5004ebc9a2e9906968c5e4
Davide Bonsembiante <115017882+bnsdavide03@users.noreply.github.com>
2023-05-14 14:29:55 +0200

    Update MIPS2000.s

--- MIPS2000.s
+++ MIPS2000.s
@@ -3,7 +3,8 @@ COMMAND:   .byte   0
 START:     .word   0x1000
 LED:       .half   0
 
-COMMAND_TABLE:
+
+ROUTINE_TABLE:
     .word   routine0_adress 
     .word   routine1_adress
     .word   routine2_adress
@@ -21,8 +22,8 @@ COMMAND_TABLE:
     .word   routine14_adress
     .word   routine15_adress    
 
-.text
 
+.text
 move $t0, 2
 
 routine0_adress:
@@ -93,34 +94,46 @@ routine15_adress:
 
 #Effettuo il complemento bit a bit dei 4 bit meno significativi
 
+
+#Core del programma
+
 main:
  li $t1, START
  li $t2, COMMAND
  li $t3, LED
+ 
+ lw $t4, START
+ beq $t4, START, controllo_comando
+ jr $ra
+
 
 #Estrazione del comando COMMAND nei 2 nybble rispettivamente più significativi e meno significativi.
 #Nel nybble meno significativi, effettuo il complemento bit a bit
 
-lb $t4, 0($t2)          # esempio:                                1110 0001  
-andi $t4, $t2, 0x0F     #Estraggo il nybble piu significativo --> 0000 0001
-andi $t5, $t2, 0xF0     #Estraggo il nybble meno significativo --> 1110 0000
+controllo_comando:
+lb $t5, 0($t2)          #Esempio:                                   1110 0001  
+andi $t5, $t2, 0xF0     #Estraggo il nybble piu significativo  -->  1110 0000
+andi $t6, $t2, 0x0F     #Estraggo il nybble meno significativo -->  0000 0001
 
-xor	$t5, $t5, $t4   # --> 1110 0001 , mi permette di fare il complemento bit a bit.
+xor	$t6, $t6, $t5       # --> 1110 0001 , mi permette di fare il complemento bit a bit.
 
-bne $t4, $t5, errore    #bisogna gestire l'errore
+bne $t5, $t6, errore
 
-
-sll $t4, $t4, 2
+sll $t5, $t5, 2
+lui $t7, ROUTINE_TABLE
+add $t7, $t7, $t5
+jr $t7
 
 
 
 # Il comando non è corretto, inibisce l'accettazione di dati per 60 secondi
 errore:
-li $t6, 60    # Carica il conteggio di 60 secondi in $t6
+li $t8, 60              # Carica il conteggio di 60 secondi in $t6
+
 
 
 # Effettua il lampeggio
 lampeggio:
-li $t0, 15000000000
+li $t.., 15000000000
 loop: addi $t0, $t0, -1
 bne $t0, $zero, loop
