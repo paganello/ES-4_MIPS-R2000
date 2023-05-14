a7061edec7085d32a6e9653c8ef9c586eefbe05b
Davide Bonsembiante <115017882+bnsdavide03@users.noreply.github.com>
2023-05-14 14:43:13 +0200

    Update MIPS2000.s

--- MIPS2000.s
+++ MIPS2000.s
@@ -92,9 +92,6 @@ routine15_adress:
     bne $t0, $zero, routine15_adress
 
 
-#Effettuo il complemento bit a bit dei 4 bit meno significativi
-
-
 #Core del programma
 
 main:
@@ -114,21 +111,17 @@ controllo_comando:
 lb $t5, 0($t2)          #Esempio:                                   1110 0001  
 andi $t5, $t2, 0xF0     #Estraggo il nybble piu significativo  -->  1110 0000
 andi $t6, $t2, 0x0F     #Estraggo il nybble meno significativo -->  0000 0001
-
 xor	$t6, $t6, $t5       # --> 1110 0001 , mi permette di fare il complemento bit a bit.
-
 bne $t5, $t6, errore
-
 sll $t5, $t5, 2         #Moltiplica il nybble più significativo per 4 per ottenere l'offset
 lui $t7, ROUTINE_TABLE  #Carica l'indirizzo della Routine_Table nel registro t7
 add $t7, $t7, $t5       #Aggiunge a t7 il nibbly più significativo per calcolare l'indirizzo della routine richiesta
 jr $t7                  #Salta all'indirizzo della routine richiesta
 
 
-# Il comando non è corretto, inibisce l'accettazione di dati per 60 secondi
-errore:
-li $t8, 60              # Carica il conteggio di 60 secondi in $t6
+# Il comando non è corretto, inibisce l'accettazione di dati per 60 secondi errore:
 
+li $t8, 60              # Carica il conteggio di 60 secondi in $t6
 
 
 # Effettua il lampeggio
